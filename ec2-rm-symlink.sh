435406dac17ac623e34cbdef460278381acbdf38
Matt Ullmer <meu@yelp.com>
2014-05-07 14:29:35 -0700

    Instead of packaging up the dependency list from hell, I did a thing and included it in line

---
+++ ec2-rm-symlink.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -e
+
+sudo rm -f /usr/local/bin/ec2-consistent-snapshot
+
---
+++ ec2-symlink.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -e
+
+sudo ln -s /usr/local/ec2-consistent-snapshot/ec2-consistent-snapshot /usr/local/bin/ec2-consistent-snapshot
+
